eed1b164bbde4dd613ede6998d18f6a76759963e
radiantegg <2173590735@qq.com>
2026-05-14 16:19:01 +0800

    token: Add token header

---
+++ include/token.h
@@ -0,0 +1,20 @@
+#ifndef TOKEN_H
+#define TOKEN_H
+
+#define ERROR_MSG_BUF_SIZE 64
+
+typedef enum {
+    TOKEN_NUMBER,
+    TOKEN_ERROR,
+    TOKEN_EOF
+} TokenType;
+
+typedef struct {
+    TokenType type;
+    double value;
+    char error[ERROR_MSG_BUF_SIZE];
+} Token;
+
+void print_token(const Token *token);
+
+#endif
